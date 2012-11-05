da72db2b8a61149e465ca92957eeb6f6ea911d3f
Kirill Gorobets <kgorob@gmail.com>
2012-11-06 01:45:47 +0400

    Added lecture 2 and practice 2.

---
+++ Lecture2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lecture2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Массив
+            int[] array = new int[10];
+            Array.Sort(array);
+
+            // Список - динамический массив
+            List<int> list = new List<int>();
+            list.Sort();
+            list.Reverse();
+
+            // Стек
+            Stack<int> stack = new Stack<int>();
+            stack.Push(100);
+            stack.Push(200);
+            stack.Push(300);
+
+            // Очередь
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(100);
+            queue.Enqueue(200);
+            queue.Enqueue(300);
+
+            // Словарь
+            var dic = new SortedDictionary<string, int>();
+            dic.Add("one", 1);
+            dic.Add("two", 2);
+            dic.Add("three", 3);
+            dic.Add("four", 4);
+            dic.Add("five", 5);
+            dic.Add("six", 6);
+            dic.Add("seven", 7);
+
+            // Демонстрация словаря
+            Console.Write("Input number: ");
+            var numberStr = Console.ReadLine().ToLower();
+            if (dic.ContainsKey(numberStr))
+            {
+                Console.WriteLine("You entered {0}", dic[numberStr]);
+            }
+            else
+            {
+                Console.WriteLine("I don't know what is {0}", numberStr);
+            }
+        }
+    }
+}
---
+++ Practice2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string LastName { get; set; }
+
+            public bool Sex { get; set; }
+        }
+
+        private static void Main(string[] args)
+        {
+            DbContext context = new DbContext("Data Source = .\\SQLEXPRESS; Initial Catalog = Test; Integrated Security = true");
+            var collection = context.Database.SqlQuery<Student>("SELECT * FROM Students").ToList();
+            foreach (var item in collection)
+            {
+                Console.WriteLine("{0} {1}, sex is {2}, ID is {3}", item.Name, item.LastName, item.Sex ? "male" : "female", item.Id);
+            }
+
+            TestEntities testContext = new TestEntities();
+
+            foreach (var item in testContext.Database.SqlQuery<Student>("SELECT * FROM Students"))
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            foreach (var item in testContext.Groups)
+            {
+                Console.WriteLine(item.Motto + ":");
+                foreach (var student in item.Students)
+                {
+                    Console.WriteLine("{0} {1} ({2})", student.Name, student.LastName, item.Head.Name);
+                }
+            }
+        }
+    }
+}
