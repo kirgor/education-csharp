a5784487b54c74763bea9ea5bb97d59af6431280
Kirill Gorobets <kgorob@gmail.com>
2013-02-01 02:12:31 +0400

    Chat project authorization.

---
+++ Chat/Chat/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using MvcApplication2.Data;
+using MvcApplication2.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login(string email, string password)
+        {
+            try
+            {
+                AuthHelper.Login(email, password);
+                Response.Cookies.Set(new HttpCookie("Email", email));
+                Response.Cookies.Set(new HttpCookie("Password", password));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string email, string password, string passwordConfirm, string name, string lastName)
+        {
+            ViewBag.Email = email;
+            ViewBag.Name = name;
+            ViewBag.LastName = lastName;
+
+            if (password != passwordConfirm)
+            {
+                ViewBag.Message = "Passwords do not match each other!";
+                return View();
+            }
+
+            try
+            {
+                AuthHelper.Register(email, password, name, lastName);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ Chat/Chat/Helpers/AuthHelper.cs
@@ -0,0 +1,61 @@
+﻿using MvcApplication2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace MvcApplication2.Helpers
+{
+    public static class AuthHelper
+    {
+        public static void Register(string email, string password, string name, string lastName)
+        {
+            using (var context = new ChatEntities())
+            {
+                if (context.Users.Any(u => u.Email == email))
+                {
+                    throw new Exception("User with such e-mail already exist!");
+                }
+                else
+                {
+                    var sha = new SHA512Managed();
+                    context.Users.Add(new User
+                    {
+                        Email = email,
+                        Password = sha.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                        Name = name,
+                        LastName = lastName
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public static User Login(string email, string password)
+        {
+            using (var context = new ChatEntities())
+            {
+                var user = context.Users.SingleOrDefault(u => u.Email == email);
+                if (user != null)
+                {
+                    var sha = new SHA512Managed();
+                    byte[] hash = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+                    for (int i = 0; i < 64; i++)
+                    {
+                        if (hash[i] != user.Password[i])
+                        {
+                            throw new Exception("Password is incorrect!");
+                        }
+                    }
+                    return user;
+                }
+                else
+                {
+                    throw new Exception("User with such e-mail doesn't exist!");
+                }
+            }
+        }
+    }
+}
